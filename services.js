cdc24ed0ae6d090dab9cf9bf6f96c1e2a2434ff0
Alf Eaton <alf@hubmed.org>
2012-08-11 01:34:57 +0100

    Use Leaflet/CloudMade for map

--- services.js
+++ services.js
@@ -20,37 +20,23 @@ var Twitter = {
 				max_id: earliest_id
 			},
 			dataType: "jsonp",
-			success: Google.addMarkers
+			success: Map.addMarkers
 		});
 	}
 };
 
-var Google = {
-	loadMapsAPI: function() {
-		var node = $("link[rel='service.google-maps']");
-
-		$.ajax({
-			url: "//maps.googleapis.com/maps/api/js",
-			data: {
-				key: "AIzaSyB9YJ7Mpo6QHAKvID1Tv5QVx4JyLpTW0IQ",
-				sensor: false,
-			},
-			dataType: "jsonp",
-			jsonpCallback: "Google.createMap"
-			//success: Google.createMap, // multiple script loads mean the random function name is gone
-		});
-	},
-
+var Map = {
 	createMap: function() {
-		var container = $("<div/>").css({ height: "100%" }).appendTo("body");
+		var container = $("<div/>", { id: "map" }).css({ height: "100%" }).appendTo("body");
 
-		var options = {
-			zoom: 8,
-			center: new google.maps.LatLng(29.95599, -90.07051),
-	        mapTypeId: google.maps.MapTypeId.ROADMAP
-	    };
+		map = L.map("map").setView([51.505, -0.09], 13);
 
-		map = new google.maps.Map(container.get(0), options);
+		var layer = L.tileLayer("http://{s}.tile.cloudmade.com/38a09cfa58ff476fb3a902cbb760720c/997/256/{z}/{x}/{y}.png", {
+		    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+		    maxZoom: 18
+		});
+
+		layer.addTo(map);
 
 		Twitter.timeline();
 	},
@@ -60,21 +46,15 @@ var Google = {
 
 		var coords, marker, earliest_id;
 
-		var bounds = new google.maps.LatLngBounds;
+		var bounds = new L.LatLngBounds;
 
 		data.forEach(function(tweet) {
 			earliest_id = tweet.id_str;
 
 			if (!tweet.geo || !tweet.geo.coordinates) return;
 
-			var point = new google.maps.LatLng(tweet.geo.coordinates[0], tweet.geo.coordinates[1]);
-
-			marker = new google.maps.Marker({
-		    	map: map,
-		    	position: point,
-		    	title: tweet.title,
-		  		animation: google.maps.Animation.DROP
-		    });
+		    var marker = L.marker(tweet.geo.coordinates).addTo(map);
+		    marker.bindPopup(tweet.title);
 
 		    bounds.extend(point);
 		});
